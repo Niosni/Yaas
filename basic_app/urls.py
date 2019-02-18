a91e5b1ab0545289434506f2e1e3787fb5e69bf3
Niklas Niemelä <niklas.niemela@hotmail.com>
2019-02-18 21:27:23 +0200

    Added bidding

--- basic_app/urls.py
+++ basic_app/urls.py
@@ -7,14 +7,15 @@ from basic_app import views
 app_name = 'basic_app'
 
 urlpatterns = [
-    path('register/',views.register,name='register'),
-    path('user_login/',views.user_login,name='user_login'),
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('profile/',views.view_profile,name='view_profile'),
+    path('profile/', views.view_profile, name='view_profile'),
     path('change-password/', views.change_password, name='change_password'),
-    path('auctions/',views.auctions,name='auctions'),
-    path('auctions/search/',views.searchAuctions,name='searchAuctions'),
-    path('auctions/add/',views.add_auction,name='add_auction'),
-    path('auctions/bid/',views.make_bid,name='make_bid'),
-    path('auctions/edit/<int:id>',views.edit_auction,name='edit_auction'),
+    path('auctions/', views.auctions, name='auctions'),
+    path('auctions/search/', views.searchAuctions, name='searchAuctions'),
+    path('auctions/add/', views.add_auction, name='add_auction'),
+    path('auctions/bid/', views.make_bid, name='make_bid'),
+    path('auctions/bid/confirm', views.confirm_bid, name='confirm_bid'),
+    path('auctions/edit/<int:id>', views.edit_auction, name='edit_auction'),
 ]
--- basic_app/views.py
+++ basic_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from basic_app.forms import UserForm,EditProfileForm,AddAuctionForm,EditAuctionForm,SearchAuctionsForm
+from basic_app.forms import UserForm, EditProfileForm, AddAuctionForm, EditAuctionForm, SearchAuctionsForm
 from basic_app.models import Auction
 
 # Extra Imports for the Login and Logout Capabilities
@@ -11,8 +11,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 
+
 def index(request):
-    return render(request,'basic_app/index.html')
+    return render(request, 'basic_app/index.html')
 
 
 @login_required
@@ -21,6 +22,7 @@ def user_logout(request):
     logout(request)
     # Return to homepage.
     return HttpResponseRedirect(reverse('index'))
+
 
 def register(request):
     registered = False
@@ -38,9 +40,10 @@ def register(request):
     else:
         user_form = UserForm()
 
-    return render(request,'basic_app/registration.html',
-                         {'user_form':user_form,
-                          'registered':registered})
+    return render(request, 'basic_app/registration.html',
+                  {'user_form': user_form,
+                   'registered': registered})
+
 
 def user_login(request):
 
@@ -54,10 +57,10 @@ def user_login(request):
 
         # If we have a user
         if user:
-            #Check it the account is active
+            # Check it the account is active
             if user.is_active:
                 # Log the user in.
-                login(request,user)
+                login(request, user)
                 # Send the user back to some page.
                 # In this case their homepage.
                 return HttpResponseRedirect(reverse('index'))
@@ -66,74 +69,116 @@ def user_login(request):
                 return HttpResponse("Your account is not active.")
         else:
             print("Someone tried to login and failed.")
-            print("They used username: {} and password: {}".format(username,password))
+            print("They used username: {} and password: {}".format(
+                username, password))
             return HttpResponse("Invalid login details supplied.")
 
     else:
-        #Nothing has been provided for username or password.
+        # Nothing has been provided for username or password.
         return render(request, 'basic_app/login.html', {})
+
 
 @login_required
 def view_profile(request):
-    args = {'user':request.user}
+    args = {'user': request.user}
     return render(request, 'basic_app/profile.html', args)
+
 
 @login_required
 def edit_profile(request):
     if request.method == 'POST':
         # First get the username and password supplied
-        form = EditProfileForm(request.POST,instance=request.user)
+        form = EditProfileForm(request.POST, instance=request.user)
 
         if form.is_valid():
             form.save()
             return render(request, 'basic_app/profile.html')
     else:
         form = EditProfileForm(instance=request.user)
-        args = {'form':form}
-        return render(request,'basic_app/edit_profile.html',args)
+        args = {'form': form}
+        return render(request, 'basic_app/edit_profile.html', args)
+
 
 @login_required
 def change_password(request):
     if request.method == 'POST':
         # First get the username and password supplied
-        form = PasswordChangeForm(data=request.POST,user=request.user)
+        form = PasswordChangeForm(data=request.POST, user=request.user)
 
         if form.is_valid():
             uusjuttu = form.save()
-            update_session_auth_hash(request,uusjuttu.user)
+            update_session_auth_hash(request, uusjuttu.user)
             return render(request, 'basic_app/profile.html')
         else:
             print("Not successful pw change")
             return render(request, 'basic_app/profile.html')
     else:
         form = PasswordChangeForm(user=request.user)
-        args = {'form':form}
-        return render(request,'basic_app/change_password.html',args)
+        args = {'form': form}
+        return render(request, 'basic_app/change_password.html', args)
+
 
 def auctions(request):
     auctions_list = Auction.objects.order_by('title')
     searchForm = SearchAuctionsForm()
-    auct_dict = {'auctions':auctions_list,'searchForm':searchForm}
-    return render(request,'basic_app/auctions.html',context=auct_dict)
+    auct_dict = {'auctions': auctions_list, 'searchForm': searchForm}
+    return render(request, 'basic_app/auctions.html', context=auct_dict)
 
 
 def add_auction(request):
+    if request.method == 'POST':
         form = AddAuctionForm(request.POST)
         if form.is_valid():
-            form.save()
+            a = Auction(author=request.user,
+                        title=request.POST['title'],
+                        description=request.POST['description'],
+                        due_date=request.POST['due_date'],
+                        price=request.POST['price'],
+                        )
+            a.save()
+
             return redirect('basic_app:auctions')
         else:
             print('NOT WORKING')
-            return render(request,'basic_app/add_auction.html',{'aaf':form})
+
+    else:
+        print(request.user)
+
+        form = AddAuctionForm(request.POST)
+        # form.clean_author()
+        return render(request, 'basic_app/add_auction.html', {'aaf': form})
 
 
-def edit_auction(request,id):
+def make_bid(request):
+    if request.method == 'POST':
+        id = request.POST['auction_id']
+        auction = Auction.objects.get(id=id)
+        print(auction)
+       # auction.price = float(auction.price) + float(0.01)
+        auct_dict = {'auction': auction}
+        # return render(request, 'basic_app/auctions.html', context=auct_dict)
 
-    print(id,'testi')
+        return render(request, 'basic_app/make_bid.html', context=auct_dict)
+
+
+def confirm_bid(request):
+    # if request.method == 'POST':
+    id = request.POST['id']
+    a = Auction.objects.get(id=id)
+    a.price = request.POST['bid_price']
+    a.save()
+
+    print(request.POST['id'])
+    return redirect('basic_app:auctions')
+
+
+def edit_auction(request, id):
+
+    print(id, 'testi')
 
     if request.method == 'GET':
         a = Auction.objects.get(id=id)
-        return render(request,'basic_app/edit_auction.html',{'description':a.description})
+        return render(request, 'basic_app/edit_auction.html', {'description': a.description})
 
     if request.method == 'POST':
         a = Auction.objects.get(id=id)
@@ -141,11 +186,12 @@ def edit_auction(request,id):
         a.description = request.POST['description']
         a.save()
         all_auctions = Auction.objects.all()
-        args={
-            'message':'Auction ' + str(id) + " desc edited",
-            'auctions':all_auctions
-            }
-        return render(request,'basic_app/auctions.html',args)
+        args = {
+            'message': 'Auction ' + str(id) + " desc edited",
+            'auctions': all_auctions
+        }
+        return render(request, 'basic_app/auctions.html', args)
+
 
 def searchAuctions(request):
     title = request.POST['title']
@@ -153,16 +199,4 @@ def searchAuctions(request):
     if request.method == 'POST':
         results = Auction.objects.filter(title__contains=title)
         print(results)
-        return render(request,'basic_app/auctions.html',{'auctions':results})
-
-def make_bid(request):
-    id = request.POST['auction_id']
-    #auction = Auction.objects.get(id)
-    print(id)
-
-
-    auctions_list = Auction.objects.order_by('title')
-    auct_dict = {'auctions':auctions_list}
-    return render(request,'basic_app/auctions.html',context=auct_dict)
-
-    return render(request,'basic_app/make_bid.html',context=auct_dict)
+        return render(request, 'basic_app/auctions.html', {'auctions': results})
